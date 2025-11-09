6cbc88acbc7562821c59facba99560b038c45be4
Kateve52911 <kateve52911@stud.noroff.no>
2025-11-09 10:49:48 +0100

    added file storage.test.js til tests folder

---
+++ tests/utils/storage.test.js
@@ -0,0 +1,6 @@
+import { describe, test } from "vitest";
+import { getUsername } from "js/utils/storage.js";
+
+describe(getUsername, () => {
+  test("testing that the function returns user name from storage", () => {});
+});
