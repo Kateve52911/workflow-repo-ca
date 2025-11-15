5780e1d65c3865ff6661d521ac7e604bb2f361ba
Kateve52911 <kateve52911@stud.noroff.no>
2025-11-15 15:16:01 +0100

    wrote e2e test for login, installed live-server and updated config files

--- playwright.config.js
+++ playwright.config.js
@@ -5,7 +5,9 @@ import { defineConfig, devices } from "@playwright/test";
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
+
+require("dotenv").config();
+
 // import path from 'path';
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
 
@@ -25,17 +27,6 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
-
-    use: {
-      baseURL: "http://localhost:5500",
-    },
-  },
 
   /* Configure projects for major browsers */
   projects: [
@@ -76,9 +67,13 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: "npm run start",
+    url: "http://localhost:5501",
+    reuseExistingServer: !process.env.CI,
+  },
+  use: {
+    baseURL: "http://localhost:5501",
+    trace: "on-first-retry",
+  },
 });
---
+++ tests/e2e/listeners/auth/loginFormListener.spec.js
@@ -0,0 +1,30 @@
+import { expect, test } from "@playwright/test";
+
+test.describe("Login", () => {
+  test("user can login with valid credentials", async ({ page }) => {
+    await page.goto("/login/");
+
+    await page.locator('input[name="email"]').fill(process.env.TEST_USER_EMAIL);
+    await page
+      .locator('input[name="password"]')
+      .fill(process.env.TEST_USER_PASSWORD);
+
+    await page.getByRole("button", { name: "login" }).click();
+
+    await expect(page.getByRole("button", { name: "logout" })).toBeVisible();
+  });
+  test("an error message is shown with invalid credentials", async ({
+    page,
+  }) => {
+    await page.goto("/login/");
+
+    await page.locator('input[name="email"]').fill(process.env.TEST_USER_EMAIL);
+    await page.locator('input[name="password"]').fill("haduffghgelknd");
+
+    await page.getByRole("button", { name: "login" }).click();
+
+    await expect(page.locator("#message-container")).toContainText(
+      "invalid email or password",
+    );
+  });
+});
