56fc237be875e5fe9a14ef4935582c603290039d
Kateve52911 <kateve52911@stud.noroff.no>
2025-11-15 10:25:15 +0100

    added baseurl to playwright config file, installed live server, and added start: live-server to package.json file

--- playwright.config.js
+++ playwright.config.js
@@ -31,6 +31,10 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
+
+    use: {
+      baseURL: "http://localhost:5500",
+    },
   },
 
   /* Configure projects for major browsers */
